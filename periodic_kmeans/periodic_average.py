0ec1528d3e351c2457139fc3040397bc13d133f5
Michael Rashkovetskyi <misharash@gmail.com>
2025-04-03 12:15:01 -0400

    Better periodic average (mean) function

---
+++ periodic_kmeans/periodic_average.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def periodic_average(a: np.ndarray[float], weights: np.ndarray[float] | None = None, period: float = 1):
+    # a must be 1D for this to work
+    if weights is None: weights = np.ones(len(a))
+    weights = weights / sum(weights) # normalize
+    a = a % period # wrap "canonically" to [0, period)
+    simple_average = np.average(a, weights = weights)
+    period_2 = period / 2
+    if a.max() - a.min() <= period_2: return simple_average # trivial case
+    # if there exists a wrapping in which the range is narrower than period/2, it's enough to try [period/2, 3 period/2) in addition to the "canonical" one, as Miniak-Górecka, Podlaski and Gwizdałła 2022 suggest, here is a shorter implementation of the algorithm
+    a2 = a + (a < period_2) * period
+    if a2.max() - a2.min() <= period_2: return np.average(a2, weights = weights) % period
+    # general case, when range is unavoidably wider than period/2
+    # first, sort a and reorder weights in the same way
+    idx = np.argsort(a)
+    a = a[idx]
+    weights = weights[idx]
+    # in the following, we try to shift elements 0 through i by a period forward and see what happens to the weighted sum of squared differences of each element with the mean
+    cumsum_w = np.cumsum(weights)
+    new_averages = simple_average + cumsum_w * period # the new mean, the elements 0<=j<=i become (a[j]+period), simply increased by period, so it's only the sum of their weights that matters
+    weighted_sums_of_squared_elements = np.sum(weights * a**2) + 2 * np.cumsum(weights * a) * period + cumsum_w * period**2 # the elements 0<=j<=i become (a[j] + period), so their squares increase by 2 a[j] period + period^2
+    weighted_sums_of_squared_differences = weighted_sums_of_squared_elements - new_averages**2 # weighted sums of (a[j] - average)^2, expands into the weighted sum of a[j]^2 - average^2
+    return new_averages[np.argmin(weighted_sums_of_squared_differences)] % period # the best average is the one that minimizes the weighted sum of squares
