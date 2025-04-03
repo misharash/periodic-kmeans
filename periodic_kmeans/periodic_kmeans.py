047ebea6cd1a6dd2433658fb37189869e5d30853
Michael Rashkovetskyi <misharash@gmail.com>
2025-04-03 13:59:10 -0700

    Fixed the double underscore name usage

--- periodic_kmeans/periodic_kmeans.py
+++ periodic_kmeans/periodic_kmeans.py
@@ -11,13 +11,12 @@ class PeriodicKMeans(kmeans):
     def __init__(self, data, period = 1, initial_centers = None, no_of_clusters = None, random_state = None):
         self.period = period
         self.period_2 = period / 2
-        _metric2 = distance_metric(type_metric.USER_DEFINED, func = self.periodic_euclidean_distance_square_numpy)
-        _centers = kmeans_plusplus_initializer(data, no_of_clusters, metric = _metric2, random_state = random_state).initialize() if initial_centers is None else initial_centers
-        _metric = distance_metric(type_metric.USER_DEFINED, func = self.periodic_euclidean_distance_numpy)
+        _metric = distance_metric(type_metric.USER_DEFINED, func = self.periodic_euclidean_distance_square_numpy)
+        _centers = kmeans_plusplus_initializer(data, no_of_clusters, metric = _metric, random_state = random_state).initialize() if initial_centers is None else initial_centers
         super().__init__(data, _centers, metric = _metric)
         # enable some numpy vectorization for distance computations
-        self.__metric.__numpy = True
-        self.__metric.__create_distance_calculator()
+        self._kmeans__metric.__numpy = True
+        self._kmeans__metric._distance_metric__create_distance_calculator()
 
 
     def periodic_euclidean_distance_square_numpy(self, object1, object2):
@@ -54,7 +53,7 @@ class PeriodicKMeans(kmeans):
             return numpy.sqrt(numpy.sum(numpy.square(diff_wrapped)))
 
 
-    def __update_centers(self):
+    def _kmeans__update_centers(self): # need to prepend parent class name to override this extra protected method
         """!
         @brief Calculate centers of clusters in line with contained objects.
         
@@ -62,11 +61,11 @@ class PeriodicKMeans(kmeans):
         
         """
         
-        dimension = self.__pointer_data.shape[1]
-        centers = numpy.zeros((len(self.__clusters), dimension))
+        dimension = self._kmeans__pointer_data.shape[1]
+        centers = numpy.zeros((len(self._kmeans__clusters), dimension))
         
-        for index in range(len(self.__clusters)):
-            cluster_points = self.__pointer_data[self.__clusters[index], :]
+        for index in range(len(self._kmeans__clusters)):
+            cluster_points = self._kmeans__pointer_data[self._kmeans__clusters[index], :]
             centers[index] = periodic_average_2d(cluster_points, axis = 0, period = self.period)
 
         return numpy.array(centers)
@@ -84,29 +83,29 @@ class PeriodicKMeans(kmeans):
         """
 
         nppoints = numpy.array(points)
-        if len(self.__clusters) == 0:
+        if len(self._kmeans__clusters) == 0:
             return []
 
-        differences = numpy.zeros((len(nppoints), len(self.__centers)))
+        differences = numpy.zeros((len(nppoints), len(self._kmeans__centers)))
         for index_point in range(len(nppoints)):
-            differences[index_point] = self.__metric(nppoints[index_point], self.__centers)
+            differences[index_point] = self._kmeans__metric(nppoints[index_point], self._kmeans__centers)
 
         return numpy.argmin(differences, axis=1)
 
 
-    def __calculate_dataset_difference(self, amount_clusters):
+    def _kmeans__calculate_dataset_difference(self, amount_clusters): # need to prepend parent class name to override this extra protected method
         """!
         @brief Calculate distance from each point to each cluster center.
 
         """
-        dataset_differences = numpy.zeros((amount_clusters, len(self.__pointer_data)))
+        dataset_differences = numpy.zeros((amount_clusters, len(self._kmeans__pointer_data)))
         for index_center in range(amount_clusters):
-            dataset_differences[index_center] = self.__metric(self.__pointer_data, self.__centers[index_center])
+            dataset_differences[index_center] = self._kmeans__metric(self._kmeans__pointer_data, self._kmeans__centers[index_center])
 
         return dataset_differences
 
 
-    def __calculate_changes(self, updated_centers):
+    def _kmeans__calculate_changes(self, updated_centers): # need to prepend parent class name to override this extra protected method
         """!
         @brief Calculates changes estimation between previous and current iteration using centers for that purpose.
 
@@ -115,12 +114,10 @@ class PeriodicKMeans(kmeans):
         @return (float) Maximum changes between centers.
 
         """
-        if len(self.__centers) != len(updated_centers):
+        if len(self._kmeans__centers) != len(updated_centers):
             maximum_change = float('inf')
-
         else:
-            changes = self.__metric(self.__centers, updated_centers)
-
+            changes = self._kmeans__metric(self._kmeans__centers, updated_centers)
             maximum_change = numpy.max(changes)
 
         return maximum_change
